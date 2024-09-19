33d46c96d3344ebf88287ba8d95698d7ea5af421
kodecraft-sam <sam@kodecraft.dev>
2024-09-19 19:05:18 +0800

    Update validate.js

--- assets/vendor/php-email-form/validate.js
+++ assets/vendor/php-email-form/validate.js
@@ -31,10 +31,10 @@
         if(typeof grecaptcha !== "undefined" ) {
           grecaptcha.ready(function() {
             try {
-              grecaptcha.execute(recaptcha, {action: 'php_email_form_submit'})
+              // grecaptcha.execute(recaptcha, {action: 'php_email_form_submit'})
               .then(token => {
                 formData.set('recaptcha-response', token);
-                php_email_form_submit(thisForm, action, formData);
+                //php_email_form_submit(thisForm, action, formData);
               })
             } catch(error) {
               displayError(thisForm, error);
